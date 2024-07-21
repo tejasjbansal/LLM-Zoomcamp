bccedfc293b1f78ad90eec7fec9fface1fad3f86
Tejash Bansal <56173595+tejasjbansal@users.noreply.github.com>
2024-07-21 18:02:41 +0530

    Update app.py

--- 4. Monitoring and Guardrails/app/app.py
+++ 4. Monitoring and Guardrails/app/app.py
@@ -32,7 +32,7 @@ def main():
     # Model selection
     model_choice = st.selectbox(
         "Select a model:",
-        ["ollama/phi3", "openai/gpt-3.5-turbo", "openai/gpt-4o", "openai/gpt-4o-mini"]
+        ["ollama/phi3","ollama/gemma:2b","Groq/llama3-8b-8192"]
     )
     print_log(f"User selected model: {model_choice}")
 
